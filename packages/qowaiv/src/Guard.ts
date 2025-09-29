36d35f7d3006bea2c0faa7e9dbb1b54c47002fd0
Corniel Nobel <corniel@gmail.com>
2025-09-29 14:13:10 +0200

    Update packages/qowaiv/src/Guard.ts

--- packages/qowaiv/src/Guard.ts
+++ packages/qowaiv/src/Guard.ts
@@ -4,7 +4,7 @@ export class Guard {
      * Guards a value to be an integer within the (optional) range.
      * @param value the value to guard.
      * @param min the (optional) minimum allowed value
-     * @param max the (optional) maximum allowed value
+     * @param max the (optional) maximum allowed (inclusive) value
      * @returns the value when an integer within the specified range.
      */
     public static int(value: unknown, min?: number, max?: number): number {
