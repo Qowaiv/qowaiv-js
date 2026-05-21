00e6aa75ffa323c5d41bc2f887c480a82f555596
Corniel Nobel <corniel@gmail.com>
2026-05-21 12:39:50 +0200

    GUID validation for zod

--- packages/qowaiv-zod/src/index.ts
+++ packages/qowaiv-zod/src/index.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod/v3';
 import { qowaivErrorMap } from './QowaivError';
 import { email } from './Email';
+import { guid } from './Guid';
 import { iban } from './Iban';
 
 z.setErrorMap(qowaivErrorMap);
 
 export const q = {
     email,
+    guid,
     iban,
 };
