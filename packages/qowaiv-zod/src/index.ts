d689245fb68e26f39751f025f4427bbf74e53769
Jur Balledux <j.balledux@tjip.com>
2026-06-22 10:02:11 +0200

    Fix build

--- packages/qowaiv-zod/src/index.ts
+++ packages/qowaiv-zod/src/index.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 import { qowaivErrorMap } from './QowaivError';
-import { email } from './Email';
-import { iban } from './Iban';
+import { email, type EmailAddressValidator } from './Email';
+import { iban, type InternationalBankAccountNumberValidator } from './Iban';
 
 z.setErrorMap(qowaivErrorMap);
 
-export const q = {
-    email,
-    iban,
-};
+export const q: {
+    email: () => EmailAddressValidator;
+    iban: () => InternationalBankAccountNumberValidator;
+} = { email, iban };
