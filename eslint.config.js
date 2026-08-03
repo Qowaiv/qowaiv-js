34a47f125d3e9f49eae615cec880bd9db709b032
Jasper de Werk <j.dewerk@tjip.com>
2026-08-03 09:51:26 +0200

    remove ignorefile listing since linting only occurs in src folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,9 @@
 import js from '@eslint/js';
-import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
-import { fileURLToPath, URL } from 'node:url';
-
-const pathToGitIgnore = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
-  includeIgnoreFile(pathToGitIgnore, {
-    gitignoreResolution: true,
-    name: 'Qowaiv .gitignore entries',
-  }),
   {
     files: ['**/*.{js,ts}'],
     extends: [
