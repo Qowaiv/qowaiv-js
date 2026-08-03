2e5b67a1373c57e056c7a52549d48ee89a7fb4f1
Jasper de Werk <j.dewerk@tjip.com>
2026-08-03 09:41:07 +0200

    add gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,23 @@
-// @ts-check
+import js from '@eslint/js';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath, URL } from 'node:url';
 
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+const pathToGitIgnore = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
+  includeIgnoreFile(pathToGitIgnore, {
+    gitignoreResolution: true,
+    name: 'Qowaiv .gitignore entries',
+  }),
   {
-    files: ["**/*.{js,ts}"],
+    files: ['**/*.{js,ts}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.strict,
       tseslint.configs.stylistic,
+      prettier,
     ],
   },
 ]);
